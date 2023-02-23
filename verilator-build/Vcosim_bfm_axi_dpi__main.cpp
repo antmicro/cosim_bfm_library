91cb021de2f4a8850297b3cefa7af01b2d6c50b3
Michał Szprejda <mszprejda@antmicro.com>
2023-02-23 18:39:41 +0100

    Prepare Verilator-Renode sample

---
+++ verilator-build/Vcosim_bfm_axi_dpi__main.cpp
@@ -0,0 +1,39 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: main() calling loop, created with Verilator --main
+
+#include "verilated.h"
+#include "Vcosim_bfm_axi_dpi.h"
+#include "../cosim_bfm_api.h"
+
+//======================
+
+int main(int argc, char** argv, char**) {
+    // Setup context, defaults, and parse command line
+    Verilated::debug(0);
+    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+    contextp->commandArgs(argc, argv);
+
+    // Construct the Verilated model, from Vtop.h generated from Verilating
+    const std::unique_ptr<Vcosim_bfm_axi_dpi> topp{new Vcosim_bfm_axi_dpi{contextp.get()}};
+
+    bfm_set_verbose(2);
+    bfm_open(0);
+    bfm_barrier(0);
+
+    // Simulate until $finish
+    while (!contextp->gotFinish()) {
+        // Evaluate model
+        topp->eval();
+        // Advance time
+        if (!topp->eventsPending()) break;
+        contextp->time(topp->nextTimeSlot());
+    }
+
+    if (!contextp->gotFinish()) {
+        VL_DEBUG_IF(VL_PRINTF("+ Exiting without $finish; no events left\n"););
+    }
+
+    // Final model cleanup
+    topp->final();
+    return 0;
+}
